f473e985cda37a080dbaad339ab0957e93befa95
yakuznecov <yakuznecov@mail.ru>
2022-08-12 09:46:03 +0300

    add expenses app

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,5 +1,6 @@
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
@@ -10,6 +11,7 @@ function ExpenseItem(props) {
 				<h2>{props.title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
 			</div>
+			<button>Change Title</button>
 		</Card>
 	);
 }
